baefaae27256b628955834aab709eabf950e901f
Alister Machado <alister.reis@gmail.com>
2024-09-06 16:13:50 +0200

    Add stress metrics

--- src/tensorflow_projection_qm/metrics/__init__.py
+++ src/tensorflow_projection_qm/metrics/__init__.py
@@ -1,4 +1,23 @@
 from .continuity import continuity
 from .trustworthiness import trustworthiness
+from .stress import (
+    normalized_stress,
+    normalized_stress_from_distances,
+    raw_stress,
+    raw_stress_from_distances,
+    scale_normalized_stress,
+    scaled_stress,
+    scaled_stress_from_distances,
+)
 
-__all__ = ["continuity", "trustworthiness"]
+__all__ = [
+    "continuity",
+    "trustworthiness",
+    "normalized_stress",
+    "normalized_stress_from_distances",
+    "raw_stress",
+    "raw_stress_from_distances",
+    "scale_normalized_stress",
+    "scaled_stress",
+    "scaled_stress_from_distances",
+]
