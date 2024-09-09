d6ee96f3f773cd9a2b10ae7309eebf6ad1c27cf0
Alister Machado <alister.reis@gmail.com>
2024-09-09 16:40:26 +0200

    Add doctests for sort_distances and enable them in GH Action

--- src/tensorflow_projection_qm/util/distance.py
+++ src/tensorflow_projection_qm/util/distance.py
@@ -15,6 +15,21 @@ def sort_distances(D) -> tf.Tensor:
     due to the negative entries added to it, but it prevents such invalid
     matrices from leaking out to the caller.
 
+    >>> D = tf.convert_to_tensor([
+    ...         [0.0, 1.0, 0.0],
+    ...         [1.0, 0.0, 0.5],
+    ...         [0.0, 0.5, 0.0]], dtype=tf.float64)
+    >>> sort_distances(D).numpy()
+    array([[0, 2, 1],
+           [1, 2, 0],
+           [2, 0, 1]], dtype=int32)
+    >>> tf.argsort(D).numpy()
+    array([[0, 2, 1],
+           [1, 2, 0],
+           [0, 2, 1]], dtype=int32)
+
+    Notice that for tf.argsort on the third row, the first element is *not* 2.
+
     Args:
         D (tf.Tensor, np.ndarray): A matrix with distances (all entries should be nonnegative)
 
@@ -22,12 +37,12 @@ def sort_distances(D) -> tf.Tensor:
         tf.Tensor: a Tensor of the same shape as D, with indices that sort the rows
         (see tf.argsort).
     """
-    return tf.argsort(D - 1.0 * tf.eye(tf.shape(D)[0], dtype=tf.float64))
+    return tf.argsort(D - 1.0 * tf.eye(tf.shape(D)[0], dtype=D.dtype))
 
 
 @tf.function
 def nearest_k(D, k) -> tuple[tf.Tensor, tf.Tensor]:
-    return tf.math.top_k(-(D - tf.eye(tf.shape(D)[0])), k=k)
+    return tf.math.top_k(-(D - tf.eye(tf.shape(D)[0], dtype=D.dtype)), k=k)
 
 
 @tf.function
