12e92baaa1bce28ef67955aa8d4cbc89c4549248
Alister Machado <alister.reis@gmail.com>
2024-09-14 18:55:22 +0200

    add pytest-cov and eager running of tf.functions when coverage testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,9 +5,16 @@ def pytest_addoption(parser: pytest.Parser) -> None:
     parser.addoption(
         "--cpu", action="store_true", default=False, help="whether to hide all GPUs from TensorFlow"
     )
+    parser.addoption(
+        "--eager", action="store_true", default=False, help="whether to run all functions eagerly"
+    )
 
 
 def pytest_collection_modifyitems(config: pytest.Config, items: list[pytest.Item]) -> None:
+    if config.getoption("--eager"):
+        import tensorflow as tf
+
+        tf.config.run_functions_eagerly(True)
     if config.getoption("--cpu"):
         import tensorflow as tf
 
