451cb8a5c0bd77dcb96e96edc88df7704a71a9d7
Alister Machado <alister.reis@gmail.com>
2024-09-15 14:24:14 +0200

    remove dead code

--- src/tensorflow_projection_qm/metrics/shepard_goodness.py
+++ src/tensorflow_projection_qm/metrics/shepard_goodness.py
@@ -6,7 +6,6 @@ from tensorflow_projection_qm.util import distance
 
 @tf.function
 def shepard_goodness_impl(X, X_2d):
-    # 1 - (6 sum d_i^2) / (n * (n**2 - 1)), d_i = rX_i - rY_i
     flat_d_high = distance.flat_psqdist(X)
     flat_d_low = distance.flat_psqdist(X_2d)
     n = tf.cast(tf.shape(flat_d_high)[0], tf.float32)
@@ -18,14 +17,6 @@ def shepard_goodness_impl(X, X_2d):
     return 1 - 6 * tf.reduce_sum(tf.math.squared_difference(ranks_high, ranks_low)) / (
         n * (n**2 - 1)
     )
-    stddev_high = tf.math.reduce_std(ranks_high, axis=-1)
-    stddev_low = tf.math.reduce_std(ranks_low, axis=-1)
-
-    mean_high = tf.reduce_mean(ranks_high, axis=-1)
-    mean_low = tf.reduce_mean(ranks_low, axis=-1)
-
-    cov = tf.reduce_sum((ranks_high - mean_high) * (ranks_low - mean_low) / n, axis=-1)
-    return cov / (stddev_high * stddev_low)
 
 
 def shepard_goodness(X, X_2d):
