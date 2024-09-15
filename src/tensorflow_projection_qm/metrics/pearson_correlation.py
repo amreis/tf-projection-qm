8e25f16cefea1a1ec94cdf78dc47b0e8fc5b36bb
Alister Machado <alister.reis@gmail.com>
2024-09-15 13:58:21 +0200

    fix Pearson Correlation and Shepard Goodness

--- src/tensorflow_projection_qm/metrics/pearson_correlation.py
+++ src/tensorflow_projection_qm/metrics/pearson_correlation.py
@@ -1,14 +1,27 @@
-import scipy.stats
 import tensorflow as tf
 
 from tensorflow_projection_qm.metrics.metric import Metric
 from tensorflow_projection_qm.util import distance
 
 
+@tf.function
+def pearson_correlation_impl(X, X_2d):
+    flat_d_high = tf.sqrt(distance.flat_psqdist(X))
+    flat_d_low = tf.sqrt(distance.flat_psqdist(X_2d))
+
+    mean_high = tf.reduce_mean(flat_d_high, axis=-1)
+    mean_low = tf.reduce_mean(flat_d_low, axis=-1)
+
+    diff_mean_high = flat_d_high - mean_high
+    diff_mean_low = flat_d_low - mean_low
+
+    return tf.reduce_sum(
+        tf.math.l2_normalize(diff_mean_high, axis=-1) * tf.math.l2_normalize(diff_mean_low, axis=-1)
+    )
+
+
 def pearson_correlation(X, X_2d):
-    flat_d_high = distance.flat_psqdist(X)
-    flat_d_low = distance.flat_psqdist(X_2d)
-    return tf.convert_to_tensor(scipy.stats.pearsonr(flat_d_high, flat_d_low)[0])
+    return pearson_correlation_impl(X, X_2d)
 
 
 class PearsonCorrelation(Metric):
--- src/tensorflow_projection_qm/metrics/shepard_goodness.py
+++ src/tensorflow_projection_qm/metrics/shepard_goodness.py
@@ -1,14 +1,35 @@
-import scipy.stats
 import tensorflow as tf
 
 from tensorflow_projection_qm.metrics.metric import Metric
 from tensorflow_projection_qm.util import distance
 
 
-def shepard_goodness(X, X_2d):
+@tf.function
+def shepard_goodness_impl(X, X_2d):
+    # 1 - (6 sum d_i^2) / (n * (n**2 - 1)), d_i = rX_i - rY_i
     flat_d_high = distance.flat_psqdist(X)
     flat_d_low = distance.flat_psqdist(X_2d)
-    return tf.convert_to_tensor(scipy.stats.spearmanr(flat_d_high.numpy(), flat_d_low.numpy())[0])
+    n = tf.cast(tf.shape(flat_d_high)[0], tf.float32)
+
+    ranks_high = tf.cast(tf.argsort(tf.argsort(flat_d_high)), tf.float32)
+    ranks_low = tf.cast(tf.argsort(tf.argsort(flat_d_low)), tf.float32)
+
+    # Assuming no ties, we can use this formula.
+    return 1 - 6 * tf.reduce_sum(tf.math.squared_difference(ranks_high, ranks_low)) / (
+        n * (n**2 - 1)
+    )
+    stddev_high = tf.math.reduce_std(ranks_high, axis=-1)
+    stddev_low = tf.math.reduce_std(ranks_low, axis=-1)
+
+    mean_high = tf.reduce_mean(ranks_high, axis=-1)
+    mean_low = tf.reduce_mean(ranks_low, axis=-1)
+
+    cov = tf.reduce_sum((ranks_high - mean_high) * (ranks_low - mean_low) / n, axis=-1)
+    return cov / (stddev_high * stddev_low)
+
+
+def shepard_goodness(X, X_2d):
+    return shepard_goodness_impl(X, X_2d)
 
 
 class ShepardGoodness(Metric):
