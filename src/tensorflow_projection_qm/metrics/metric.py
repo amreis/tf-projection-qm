2c55eb710eb255087025fb14cde38e585db32827
Alister Machado <alister.reis@gmail.com>
2024-09-16 15:54:03 +0200

    Split MetricSet method to avoid TF retracing

--- src/tensorflow_projection_qm/metrics/metric.py
+++ src/tensorflow_projection_qm/metrics/metric.py
@@ -1,6 +1,8 @@
 from abc import ABC, abstractmethod
 from typing import Iterable, TypeVar
 
+import tensorflow as tf
+
 # typing.Self only available in Python>=3.11.
 TLocalizableMetric = TypeVar("TLocalizableMetric", bound="LocalizableMetric")
 TMetric = TypeVar("TMetric", bound="Metric")
@@ -49,8 +51,17 @@ class MetricSet:
     def set_default(self, **kwargs):
         self.defaults |= kwargs
 
-    def measure_from_dict(self, data_dict):
+    @tf.function
+    def _measure(self, X, X_2d, y=None):
         return {
-            m.name: m.set_if_missing(self.defaults).measure_from_dict(data_dict)
+            m.name: m.set_if_missing(self.defaults).measure_from_dict(
+                {"X": X, "X_2d": X_2d, "y": y}
+            )
             for m in self.metrics
         }
+
+    def measure_from_dict(self, data_dict: dict):
+        # We split the dict and call the underlying measure_from_dict
+        # so that we can annotate _measure with @tf.function without
+        # causing frequent retracing.
+        return self._measure(data_dict["X"], data_dict["X_2d"], data_dict.get("y"))
