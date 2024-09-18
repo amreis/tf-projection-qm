a4ec4ffd810646a9810129696ad5d969c3274773
Alister Machado <alister.reis@gmail.com>
2024-09-18 17:58:10 +0200

    Optimize the implementation of Procrustes
    The big change here is dropping the explicit centering matrix H
    in favor of subtracting the appropriate means. This is a nice
    memory saving because of the intermediate matrices created by
    the previous use of tf.einsum.

--- src/tensorflow_projection_qm/metrics/procrustes.py
+++ src/tensorflow_projection_qm/metrics/procrustes.py
@@ -1,4 +1,23 @@
+"""Implementation of the Procrustes quality metric.
+
+Described in "Local procrustes for manifold embedding: a measure of embedding quality
+and embedding algorithms", by Goldbert and Ritov (2009).
+
+The implementation below reflects equation (7), where the procedure aims to "align" the
+high and low dimensional spaces with rotation, translation, *and* scaling. Since this
+support for scaling is aimed at conformal maps, we might drop it or make it optional.
+In any case, the public API is the same.
+
+In the paper, you'll see the matrix H being used quite often. H is a centering matrix
+(i.e., an idempotent matrix that makes the mean of the points it's applied to become 0).
+We have explicitly used mean subtraction here as it's more space and time efficient.
+Also, the paper uses tr(X'X) and ||X||_F^2 (which are the same thing); while we could use
+tensorflow's `norm` function, we choose to just square and add the elements of the matrices,
+as that's what's really happening in this notation.
+"""
+
 from typing import Optional
+
 import tensorflow as tf
 
 from tensorflow_projection_qm.metrics.metric import LocalizableMetric
@@ -7,36 +26,38 @@ from tensorflow_projection_qm.util import distance
 
 @tf.function
 def procrustes_impl(X, X_2d, k):
-    # TODO(amreis): when adding docs, please note that we support comparing to
-    # a rotated *and rescaled* version of the 2D embedding.
     n = tf.shape(X)[0]
     k = tf.minimum(k, n)
-
-    H = tf.eye(k, dtype=X.dtype)
-    H -= tf.ones_like(H) / tf.cast(k, H.dtype)
-
     knn_orig = distance.nearest_k(distance.psqdist(X), k=k)[1]
     # batch of k-nearest neighbors per point, including the point itself.
     data_neighborhoods = tf.gather(X, knn_orig)  # shape = (n, k, data_dim)
     # ZADU's implementation uses knn_proj here and it's *wrong*. They're
     # comparing two different sets of points. We compare the same set of
-    # points. The data neighborhoods and their embeddings.
+    # points. The data neighborhoods and *their* embeddings.
     proj_neighborhoods = tf.gather(X_2d, knn_orig)  # shape = (n, k, proj_dim)
 
-    Z = tf.linalg.einsum("bij,il,blm->bjm", data_neighborhoods, H, proj_neighborhoods)
+    Z = tf.matmul(
+        data_neighborhoods,
+        proj_neighborhoods - tf.reduce_mean(proj_neighborhoods, -2, keepdims=True),
+        transpose_a=True,
+    )
     s_i, u_i, v_i = tf.linalg.svd(Z)
-    c_i = tf.reduce_sum(s_i, -1) / tf.norm(proj_neighborhoods, ord="fro", axis=(-2, -1)) ** 2
+
+    c_i = tf.reduce_sum(s_i, -1) / tf.reduce_sum(proj_neighborhoods**2, axis=(-2, -1))
 
     A_i = tf.matmul(u_i, v_i, transpose_b=True)
     procrustes_error = data_neighborhoods - tf.matmul(
         proj_neighborhoods, c_i[:, tf.newaxis, tf.newaxis] * A_i, transpose_b=True
     )
-    procrustes_i = (
-        tf.norm(tf.matmul(H[tf.newaxis, ...], procrustes_error), ord="fro", axis=(-2, -1)) ** 2
+    procrustes_i = tf.reduce_sum(
+        (procrustes_error - tf.reduce_mean(procrustes_error, -2, keepdims=True)) ** 2,
+        axis=(-2, -1),
     )
 
-    centered_data_neighs = tf.matmul(H[tf.newaxis, ...], data_neighborhoods)
-    denom = tf.norm(centered_data_neighs, ord="fro", axis=(-2, -1)) ** 2
+    centered_data_neighs = data_neighborhoods - tf.reduce_mean(
+        data_neighborhoods, -2, keepdims=True
+    )
+    denom = tf.maximum(tf.reduce_sum(centered_data_neighs**2, axis=(-2, -1)), 1e-12)
 
     return procrustes_i / denom
 
